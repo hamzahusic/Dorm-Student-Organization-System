22ce14ccf08f4295f4a6ee107c31046ccaaec71d
Hamza Husic <hamzahusic.dev@gmail.com>
2025-10-15 10:08:35 +0200

    Fixed dashboard sidebar toggle button

--- frontend/assets/js/dashboard-script.js
+++ frontend/assets/js/dashboard-script.js
@@ -7,19 +7,30 @@
 // Scripts
 // 
 
-const sideBar = []
+const sideBars = []
 
 const sideBarToggleScript = () => {
     // Toggle the side navigation
-    const sidebarToggle = document.body.querySelector('#sidebarToggle');
-    if (sidebarToggle && !sideBar.includes(sidebarToggle)) {
+
+    const sidebarToggle = document.body.querySelectorAll('#sidebarToggle');
+
+    if (sidebarToggle.length > 0) {
+
         if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
             document.body.classList.toggle('sb-sidenav-toggled');
         }
-        sidebarToggle.addEventListener('click', event => {
-            event.preventDefault();
-            document.body.classList.toggle('sb-sidenav-toggled');
-            localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
-        });
+
+        
+        sidebarToggle.forEach((sideBar) => {
+            if(sideBars.includes(sideBar)) return;
+
+            sideBar.addEventListener('click', event => {
+                event.preventDefault();
+                document.body.classList.toggle('sb-sidenav-toggled');
+                localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
+                sideBars.push(sideBar);
+            });
+        })
+
     }
 }
